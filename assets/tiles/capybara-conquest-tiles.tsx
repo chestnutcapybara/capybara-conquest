0b869bf6fca0d78008a4cd8475015247254b7391
Hongyi Xiao <249873180+chestnutcapybara@users.noreply.github.com>
2026-05-04 20:21:51 -0700

    Start creating chunks in Tiled * Create a new project in Tiled * Add all tiles assets to a Tileset (tsx file) * Create a new Map which has the most basic Chunk: a platform

---
+++ assets/tiles/capybara-conquest-tiles.tsx
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="capybara-conquest-tiles" tilewidth="320" tileheight="320" tilecount="30" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Bonus.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Brick 01.png" width="128" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Brick 02.png" width="128" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Bridge Part 01.png" width="64" height="64"/>
+ </tile>
+ <tile id="4">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Bridge Part 02.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 01.png" width="128" height="128"/>
+ </tile>
+ <tile id="6">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 02.png" width="128" height="128"/>
+ </tile>
+ <tile id="7">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 03.png" width="128" height="128"/>
+ </tile>
+ <tile id="8">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 04.png" width="128" height="128"/>
+ </tile>
+ <tile id="9">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 05.png" width="128" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 06.png" width="128" height="128"/>
+ </tile>
+ <tile id="11">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 07.png" width="128" height="128"/>
+ </tile>
+ <tile id="12">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 08.png" width="128" height="128"/>
+ </tile>
+ <tile id="13">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 09.png" width="128" height="128"/>
+ </tile>
+ <tile id="14">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 10.png" width="128" height="128"/>
+ </tile>
+ <tile id="15">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 11.png" width="128" height="128"/>
+ </tile>
+ <tile id="16">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 12.png" width="128" height="128"/>
+ </tile>
+ <tile id="17">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground 13.png" width="128" height="128"/>
+ </tile>
+ <tile id="18">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground Additional 01.png" width="320" height="320"/>
+ </tile>
+ <tile id="19">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground Additional 02.png" width="320" height="320"/>
+ </tile>
+ <tile id="20">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground Additional 03.png" width="320" height="320"/>
+ </tile>
+ <tile id="21">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground Additional 04.png" width="320" height="320"/>
+ </tile>
+ <tile id="22">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground Additional 05.png" width="128" height="320"/>
+ </tile>
+ <tile id="23">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground Additional 06.png" width="128" height="320"/>
+ </tile>
+ <tile id="24">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ground Additional 07.png" width="320" height="320"/>
+ </tile>
+ <tile id="25">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Ladder.png" width="128" height="128"/>
+ </tile>
+ <tile id="26">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Spike.png" width="128" height="128"/>
+ </tile>
+ <tile id="27">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Stone.png" width="128" height="128"/>
+ </tile>
+ <tile id="28">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Wooden Barrel.png" width="128" height="128"/>
+ </tile>
+ <tile id="29">
+  <image source="Autumn_Forest_2D_Platformer_Tileset_Platformer - Wooden Box.png" width="129" height="128"/>
+ </tile>
+</tileset>
