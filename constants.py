b3e0503b6b562f29b67f6802928f3d5db518acf3
Hongyi Xiao <249873180+chestnutcapybara@users.noreply.github.com>
2026-05-05 20:24:04 -0700

    Made screen height less

--- constants.py
+++ constants.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     raise CapybaraConquestError("This file is not meant to be run directly, it contains constants for the game. Please run main.py instead.")
 
 BACKGROUNDCOLOR = (237, 199, 154)
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 1200
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 900
 FONT = pygame.font.Font("assets/fonts/Capybara.ttf", 96)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
