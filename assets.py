44c029a6447b44433118a00848e87c20e4d7d6bd
Hongyi Xiao <249873180+chestnutcapybara@users.noreply.github.com>
2026-05-08 07:12:33 -0700

    Change resolve_path because we aren't using PyInstaller so there's no reason to look for _MEIPASS, etc

--- assets.py
+++ assets.py
@@ -1,15 +1,13 @@
 from pathlib import Path
-from typing import Any
 import sys
-def resolve_path(relative_path: str) -> Path:
-    if hasattr(sys, "_MEIPASS"):
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = Path(getattr(sys, "_MEIPASS"))
-    else:
-        # In development, use the current working directory
-        base_path = Path(".").resolve()
 
-    return base_path / Path(relative_path)
+def resolve_path(relative_path: str) -> Path:
+    if getattr(sys, "frozen", False):
+        base_path = Path(sys.argv[0]).resolve().parent
+    else:
+        base_path = Path(__file__).resolve().parent
+
+    return base_path / relative_path
 
 class LoadAssets:
     pass
